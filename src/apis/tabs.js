43758f5360adc388f8f1fa7d6569b25d31296651
Matt Swieboda <mswiebod@gmail.com>
2020-06-17 16:21:56 -0500

    fixes tab api call to encode spaces and question marks

--- src/apis/tabs.js
+++ src/apis/tabs.js
@@ -2,7 +2,12 @@ import axios from 'axios';
 
 const baseUrl = 'http://tabster.herokuapp.com';
 const getNewest = () => axios.get(`${baseUrl}/api/tabs?sort=newest`);
-const getTab = (artist, title) => axios.get(`${baseUrl}/api/tabs/${artist}/${title}`);
+const getTab = (artist, title) => {
+  title = title.replace(/\s/g, '+').replace(/\?/g, "%3F");
+  artist = artist.replace(/\s/g, '+').replace(/\?/g, "%3F");
+
+  return axios.get(`${baseUrl}/api/tabs/${artist}/${title}`);
+};
 
 export {
   getNewest,
